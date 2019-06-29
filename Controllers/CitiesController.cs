85296b3567695f9dfd54aec11f9164506b9d524c
Imlebowski <Imlebowski@users.noreply.github.com>
2019-06-29 11:16:11 +0530

    Added gitignore

--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -2,17 +2,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfo.API.Controllers
 {
+    [Route("api/theDude")]
     public class CitiesController : Controller
     {
-        [Route("api/theDude")]
         [HttpGet]
         public JsonResult GetCities()
         {
-            return new JsonResult(new {
+            return new JsonResult(new
+            {
                 Id = 1,
                 Name = "Bengaluru",
                 Desc = "Garden City"
-            }); 
+            });
+        }
+        
+        [Route("{id}")]
+        [HttpGet]
+        public JsonResult GetCity(int CityID)
+        {
+            return new JsonResult(new
+            {
+                Id = 1,
+                Name = "Bengaluru",
+                Desc = "Garden City"
+            });
         }
     }
 }
